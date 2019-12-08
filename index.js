f82628166e999844ee242476b58570dce4db5ae4
Bilal Ahmed <45879053+bill-ahmed@users.noreply.github.com>
2019-12-08 16:13:54 -0500

    cleanup unused code for Express

--- index.js
+++ index.js
@@ -7,14 +7,6 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 9000;
 
-/* Setup and send twilio SMS messages  */
-// Authorization
-// var accountSid = twilioConnection.twilio.accountSid;
-// var authToken = twilioConnection.twilio.authToken;
-
-// Setup client
-// var client = new Twilio(accountSid, authToken);
-
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
@@ -41,27 +33,3 @@ if(process.env.NODE_ENV === 'production'){
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'client/public/index.html'));
 })
-
-
-/* Create a POST route for sending SMS to a number req.recipients with body req.smsBody */
-// app.post('/_api/sendSMS', bodyParser.json(), (req, res) => {
-//   console.log(req.body.recipients);
-//   res.header("Content-Type", "application/json");
-
-//   // Call twilio API
-//   for(let i = 0; i < req.body.recipients.length; i++){
-//     try{
-//       client.messages.create({
-//         body: req.body.smsBody,
-//         to: req.body.recipients[i],
-//         from: "+16473608497"
-//       }).then((message) => {res.json({ "twilioResponse": message, "ok": true })});
-//     } catch(e){
-//       console.log(e);
-//       res.json({"Error" : e, "ok": false});
-//     }
-//   }
-
-// });
-
-
